e2a838d0c83dd71f837d68ba99f51ae1dbb24a53
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-10-29 18:11:15 +0900

    feat: main 화면 생성

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,15 +1,49 @@
 import styled from 'styled-components';
 
+import Button from '@@components/Button';
+import Flex from '@@components/Flex';
 import FullScreen from '@@components/FullScreen';
 import Header from '@@components/Header';
+import Typography from '@@components/Typography';
 
-const StyledMain = styled(FullScreen)``;
+const StyledMain = styled(FullScreen)`
+  .body {
+    padding: 0 30px;
+
+    .main__top {
+      flex: 1;
+      gap: 12px;
+    }
+
+    .main__bottom {
+      flex: 1;
+
+      .main__button_wrap {
+        display: flex;
+      }
+    }
+  }
+`;
 
 function Main() {
   return (
     <StyledMain>
       <Header hiddenBack>AI 특허 파일럿</Header>
-      <div className='body'></div>
+      <Flex.Vertical className='body'>
+        <Flex.Horizontal className='main__top' alignItems='center' gap={12}>
+          <Typography.LargeTitle className='main__title'>
+            아이디어의 가치를
+            <br />
+            높이세요.
+          </Typography.LargeTitle>
+        </Flex.Horizontal>
+        <Flex.Vertical className='main__bottom'>
+          <Flex.Vertical className='main__button_wrap' gap={12}>
+            <Button.Large>로그인</Button.Large>
+            <Button.Large theme='secondary'>회원가입</Button.Large>
+          </Flex.Vertical>
+        </Flex.Vertical>
+      </Flex.Vertical>
     </StyledMain>
   );
 }
