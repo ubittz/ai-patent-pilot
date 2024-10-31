c67f89b6e6fae7963a59f2e7369cca4ca2e7a2de
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-10-31 09:13:35 +0900

    fix: Button Theme에 세미콜론 추가

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -19,11 +19,11 @@ export const theme: DefaultTheme = {
       [BUTTON_SIZE.SMALL]: 'height: 32px;',
     },
     theme: {
-      [BUTTON_THEME.PRIMARY]: `background: ${COLORS.MAIN_400}; border: none; color: ${COLORS.GRAY_SCALE_000}`,
-      [BUTTON_THEME.SECONDARY]: `background: ${COLORS.GRAY_SCALE_000}; border: 1px solid ${COLORS.MAIN_400}; color: ${COLORS.MAIN_400}`,
-      [BUTTON_THEME.SOFT]: `background: ${COLORS.MAIN_050}; border: none; color: ${COLORS.MAIN_700}`,
-      [BUTTON_THEME.OUTLINE]: `background: ${COLORS.GRAY_SCALE_000}; border: 1px solid ${COLORS.GRAY_SCALE_050}; color: ${COLORS.GRAY_SCALE_700}`,
-      [BUTTON_THEME.INACTIVE]: `background: ${COLORS.GRAY_SCALE_100}; border: none; color: ${COLORS.GRAY_SCALE_200}`,
+      [BUTTON_THEME.PRIMARY]: `background: ${COLORS.MAIN_400}; border: none; color: ${COLORS.GRAY_SCALE_000};`,
+      [BUTTON_THEME.SECONDARY]: `background: ${COLORS.GRAY_SCALE_000}; border: 1px solid ${COLORS.MAIN_400}; color: ${COLORS.MAIN_400};`,
+      [BUTTON_THEME.SOFT]: `background: ${COLORS.MAIN_050}; border: none; color: ${COLORS.MAIN_700};`,
+      [BUTTON_THEME.OUTLINE]: `background: ${COLORS.GRAY_SCALE_000}; border: 1px solid ${COLORS.GRAY_SCALE_050}; color: ${COLORS.GRAY_SCALE_700};`,
+      [BUTTON_THEME.INACTIVE]: `background: ${COLORS.GRAY_SCALE_100}; border: none; color: ${COLORS.GRAY_SCALE_200};`,
     },
   },
   typography: {
